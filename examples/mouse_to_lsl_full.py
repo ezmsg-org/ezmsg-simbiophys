f1dc4d79d1dfc9c0d4ced2a4c785b05d34c1d349
Chadwick Boulay <chadwick.boulay@gmail.com>
2025-12-30 09:19:52 -0500

    MousePoller must be driven by Clock

--- examples/mouse_to_lsl_full.py
+++ examples/mouse_to_lsl_full.py
@@ -84,6 +84,7 @@ def main(
         "SINK": LSLOutletUnit(LSLOutletSettings(stream_name="MouseModulatedRaw", stream_type="ECEPhys")),
     }
     conns = (
+        (comps["CLOCK"].OUTPUT_SIGNAL, comps["SOURCE"].INPUT_SIGNAL),
         (comps["SOURCE"].OUTPUT_SIGNAL, comps["DIFF"].INPUT_SIGNAL),
         (comps["DIFF"].OUTPUT_SIGNAL, comps["ENCODER"].INPUT_SIGNAL),
         (comps["ENCODER"].OUTPUT_SIGNAL, comps["SINK"].INPUT_SIGNAL),
